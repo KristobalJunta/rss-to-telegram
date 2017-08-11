47dc5de76e409fff560a78964a1efd2453b4b4a1
Kristobal <junta.kristobal@gmail.com>
2017-08-12 01:15:18 +0300

    implemented basic bot functionality

--- main.py
+++ main.py
@@ -2,9 +2,49 @@
 
 from config import config
 import feedparser
-import requests
-import os
 import telebot
+import json
+import re
+import os
+
 
 bot = telebot.TeleBot(config.get('telegram-token'))
-bot.send_message(config.get('channel-id'), "Hello from the bot!")
+feed = feedparser.parse(config.get('feed'))
+new_posts = []
+
+if not os.path.exists('posts.json'):
+    with open('posts.json', 'w') as f:
+        json.dump(f, [])
+        f.close()
+
+with open('posts.json') as f:
+    guids = json.load(f)
+    f.close()
+
+for entry in feed.entries:
+    content = entry['description']
+    image_url = re.search('src=".*"', content)
+
+    if image_url:
+        image_url = image_url.group(0).strip('"').strip('src="')
+
+    brpos = content.index("<br />")
+    text = content[brpos+6:].replace('<br /><br />', '\n').replace('<br />', '\n')
+    post = {}
+    if image_url:
+        post['image'] = image_url
+    post['text'] = text
+    if entry['guid'] not in guids:
+        new_posts.append(post)
+        guids.append(entry['guid'])
+
+new_posts.reverse()
+
+for post in new_posts:
+    bot.send_message(config.get('channel-id'), post.get('text'))
+    if post.get('image'):
+        bot.send_photo(config.get('channel-id'), post.get('image'))
+
+with open('posts.json', 'w') as f:
+    json.dump(guids, f)
+    f.close()
