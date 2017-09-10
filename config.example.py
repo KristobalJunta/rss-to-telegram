31bcad157dce1571719dd1570d9cd36612ad73a0
Kristobal <junta.kristobal@gmail.com>
2017-09-11 00:16:12 +0300

    added ability to supply several feeds in config

--- config.example.py
+++ config.example.py
@@ -6,5 +6,5 @@ Copy this to config.py and fill in your actual data before starting
 config = {
     "telegram-token": "your telegram bot token",
     "channel-id": "telegram channel to post to",
-    "feed": "RSS feed url to get data from"
+    "feeds": ["RSS feed urls", "to get data from"]
 }
--- main.py
+++ main.py
@@ -1,15 +1,28 @@
 # _*_ coding: utf-8 _*_
 
-from config import config
 from bs4 import BeautifulSoup
+from datetime import datetime
+from config import config
 import feedparser
 import telebot
 import json
 import os
 
 
+def get_timestamp(dt):
+    return datetime.strptime(dt, '%a, %d %b %Y %H:%M:%S %z').timestamp()
+
+
 bot = telebot.TeleBot(config.get('telegram-token'))
-feed = feedparser.parse(config.get('feed'))
+
+feeds = list()
+
+if type(config.get('feeds')) == str:
+    feeds.append(feedparser.parse(config.get('feeds')))
+else:
+    for feed in config.get('feeds'):
+        feeds.append(feedparser.parse(feed))
+
 new_posts = []
 
 if not os.path.exists('posts.json'):
@@ -21,19 +34,22 @@ with open('posts.json') as f:
     guids = json.load(f)
     f.close()
 
-for entry in feed.entries:
-    content = entry['description']
-    post = {}
-    soup = BeautifulSoup(content, 'html.parser')
-    if soup.img:
-        post['image'] = soup.img['src']
-    post['text'] = '\n'.join(soup.stripped_strings)
-    post['text'] += '\n\n{}'.format(entry['link'])
-    if entry['guid'] not in guids:
-        new_posts.append(post)
-        guids.append(entry['guid'])
+for feed in feeds:
+    for entry in feed.entries:
+        content = entry['description']
+        post = {}
+        soup = BeautifulSoup(content, 'html.parser')
+        if soup.img:
+            post['image'] = soup.img['src']
+        post['text'] = '\n'.join(soup.stripped_strings)
+        post['text'] += '\n\n{}'.format(entry['link'])
+        post['date'] = get_timestamp(entry['published'])
 
-new_posts.reverse()
+        if entry['guid'] not in guids:
+            new_posts.append(post)
+            guids.append(entry['guid'])
+
+new_posts.sort(key=lambda x: x['date'])
 
 for post in new_posts:
     bot.send_message(config.get('channel-id'), post.get('text'))
